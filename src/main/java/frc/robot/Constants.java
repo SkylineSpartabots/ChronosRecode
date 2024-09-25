76bb5fa2add472750f28e31f3063c6e944db15c0
Ethan <ethanmwgo@gmail.com>
2024-09-24 18:15:13 -0700

    initial structure

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,16 @@
+package frc.robot;
+
+public final class Constants {
+    
+    public static double MaxSpeed = 4.5; //random number, need to get actual value
+    public static double MaxAngularRate = 2 * Math.PI; //
+
+    public static final double stickDeadband = 0.15;
+    public static final double triggerDeadzone = 0.2;
+
+    public static final class HardwarePorts {
+        
+        public static final int intakeTopM = 0;
+        
+    }
+}
