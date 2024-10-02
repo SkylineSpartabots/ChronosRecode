68ab426c16b6b34f1c2de73d5f0b7068f13b529a
nat-tri1 <natiey1@outlook.com>
2024-10-01 19:14:43 -0700

    motor ids

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -10,8 +10,27 @@ public final class Constants {
 
     public static final class HardwarePorts {
         
-        public static final int intakeM = 0;
+        public static final int frontLeftSteer = 1;
+        public static final int frontLeftDrive = 2;
 
+        public static final int backLeftSteer = 3;
+        public static final int backLeftDrive = 4;
+
+        public static final int backRightSteer = 5;
+        public static final int backRightDrive = 6;
+
+        public static final int frontRightSteer = 7;
+        public static final int frontRightDrive = 8;
+
+        public static final int intakeM = 11;
+
+        public static final int leftIndexerM = 21;
+        public static final int rightIndexerM = 22;
+
+        public static final int leftShooterM = 31;
+        public static final int rightShooterM = 32;
+        
+        // public static final int hoodM = 41;
     }
 
     public static final class CurrentLimits {
