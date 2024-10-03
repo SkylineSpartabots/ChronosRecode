d6f1d0e3ee01523b04fbed40c0a0ba9dad1997cb
eggliot <eharhar06@proton.me>
2024-10-02 21:45:54 -0700

    making getters setters consistent accross susbsystems; formatting

--- src/main/java/frc/robot/subsystems/Indexer.java
+++ src/main/java/frc/robot/subsystems/Indexer.java
@@ -15,59 +15,78 @@ import frc.robot.Constants;
 
 
 public class Indexer extends SubsystemBase {
-  private final TalonFX m_Indexer;
+    private final TalonFX m_Indexer;
 
-  /** Creates a new Indexer. */
-  public Indexer() {
-    m_Indexer = new TalonFX(Constants.HardwarePorts.indexerM); //TODO set the correct ID
+    private IndexerStates currentState = IndexerStates.OFF;
+    
+    /**
+     * Creates a new Indexer.
+     */
+    public Indexer() {
+        m_Indexer = new TalonFX(Constants.HardwarePorts.indexerM); //TODO set the correct ID
 
-    m_Indexer.setInverted(false); // no idea
-    configMotor(m_Indexer);
-  }
-  
-  private void configMotor(TalonFX motor){
+        m_Indexer.setInverted(false); // no idea
+        configMotor(m_Indexer);
+    }
+
+    private void configMotor(TalonFX motor) {
         TalonFXConfiguration config = new TalonFXConfiguration();
         config.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
         config.MotorOutput.NeutralMode = NeutralModeValue.Brake;
 
         Slot0Configs slot0Configs = new Slot0Configs();
-        slot0Configs.withKP(0.4); 
+        slot0Configs.withKP(0.4);
 
         motor.getConfigurator().apply(config);
         motor.getConfigurator().apply(slot0Configs);
-  }
-  
-  public enum IndexerStates {
-    INTAKE(0.75),
-    INDEX(0.7),
-    OFF(0),
-    REV(-0.5);
-
-    final private double speed;
-    public double getValue() {
-        return speed;
     }
 
-    IndexerStates(double speed) {
-        this.speed = speed;
+    public enum IndexerStates {
+        INTAKE(0.75),
+        INDEX(0.7),
+        OFF(0),
+        REV(-0.5);
+
+        final private double speed;
+
+        public double getValue() {
+            return speed;
+        }
+
+        IndexerStates(double speed) {
+            this.speed = speed;
+        }
     }
-  }
-
-  /**
-   * Set the speed of the motor.
-   * @param percentageOutput Range from -1 to 1
-   */
-  public void setSpeed(double percentageOutput){
-    m_Indexer.set(percentageOutput);
-  }
-  
-  public void setSpeed(IndexerStates state) {
-    m_Indexer.set(state.getValue());
-  }
 
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+    public IndexerStates getCurrentState() {
+        return currentState;
+    }
+
+    public double getSpeed() {
+        return m_Indexer.get();
+    }
+
+    public double getVoltage() {
+        return m_Indexer.getMotorVoltage().getValueAsDouble();
+    }
+
+    public void setCurrentState(IndexerStates state) {
+        currentState = state;
+        m_Indexer.set(state.speed);
+    }
+
+    public void setSpeed(double speed) {
+        m_Indexer.set(speed);
+    }
+
+    public void setVoltage(double voltage) {
+        m_Indexer.setVoltage(voltage);
+    }
+
+
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+    }
 }
