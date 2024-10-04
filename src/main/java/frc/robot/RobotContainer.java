a3c484cd7373c62e62f56810b51c205f19883c9a
Darkle <120344717+Dark-IsHere@users.noreply.github.com>
2024-10-03 18:51:07 -0700

    Robot container changes; Swerve good!

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,21 +4,41 @@
 
 package frc.robot;
 
+import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.DriveRequestType;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.CommandSwerveDrivetrain;
 
 
 
 public class RobotContainer {
 
+  private final CommandSwerveDrivetrain drivetrain = CommandSwerveDrivetrain.getInstance(); // Drivetrain
+
   double translationDeadband = 0.05;
   double rotDeadband = 0.05;
+
+    private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
+            .withDeadband(Constants.MaxSpeed * translationDeadband).withRotationalDeadband(Constants.MaxAngularRate * rotDeadband)
+            .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // I want field-centric
+  
+    public final CommandXboxController driver = new CommandXboxController(0); // Driver joystick
 
   public RobotContainer() {
     configureBindings();
   }
 
-  private void configureBindings() {}
+  
+
+  private void configureBindings() {
+
+    drivetrain.setDefaultCommand( // Drivetrain will execute this command periodically
+    drivetrain.applyRequest(() -> drivetrain.drive(-driver.getLeftY(), -driver.getLeftX(), -driver.getRightX()) // Drive counterclockwise with negative X (left)
+    ));
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
