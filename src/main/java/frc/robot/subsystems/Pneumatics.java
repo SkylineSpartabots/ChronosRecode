91902087d2d621c90fa5841616f70650fb331e8a
Ethan <ethanmwgo@gmail.com>
2024-11-06 10:29:29 -0800

    various pneumatical and solenoidal affairs

--- src/main/java/frc/robot/subsystems/Pneumatics.java
+++ src/main/java/frc/robot/subsystems/Pneumatics.java
@@ -3,11 +3,13 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import frc.robot.Constants;
 
 public class Pneumatics extends SubsystemBase { 
+    //note: might be possible to get rid of compressor object, should activate automatically when we use the solenoid
     private static Pneumatics instance;
     private Compressor compressor;
     private Solenoid solenoid;
@@ -22,13 +24,53 @@ public class Pneumatics extends SubsystemBase {
     public Pneumatics() {
         compressor = new Compressor(Constants.PneumaticIDs.pneumaticsHub, PneumaticsModuleType.REVPH);
         solenoid = new Solenoid(Constants.PneumaticIDs.pneumaticsHub, PneumaticsModuleType.REVPH, Constants.PneumaticIDs.solenoid);
-
-        compressor.enableDigital();
         
     }
     
     public void setSolenoid(boolean state) {
         solenoid.set(state);
+    }
+
+    public void enableCompressor(){
+        compressor.enableDigital();
+    }
+
+    public void disableCompressor(){
+        compressor.disable();
+    }
+
+    //solenoid gets disabled until next power cycle if shorted
+    public boolean solenoidDisabled(){
+        return solenoid.isDisabled();
+    }
+
+    public boolean compressorEnabled() {
+        return compressor.isEnabled();
+    }
+
+
+    //according to documentation returns true if system NOT full, false if system full
+    public boolean getCompressorSwitchState(){
+        return compressor.getPressureSwitchValue();
+    }
+
+    public double getCompressorCurrent(){
+        return compressor.getCurrent();
+    }
+
+    public double getPressure(){
+        return compressor.getPressure();
+    }
+
+    @Override
+    public void periodic() {
+        if(getCompressorSwitchState()){
+            enableCompressor();
+        } else{
+            disableCompressor();
+        }
+
+        SmartDashboard.putNumber("available pressure", getPressure());
     }
 
 }
