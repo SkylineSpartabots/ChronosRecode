00f8c1aac22b7b99d1fab7475dd76f1dd9f3c781
eggliot <eharhar06@proton.me>
2024-11-06 03:52:10 -0800

    starting pneumatics

--- src/main/java/frc/robot/commands/CommandFactory.java
+++ src/main/java/frc/robot/commands/CommandFactory.java
@@ -16,7 +16,7 @@ public class CommandFactory {
 
     public static Command Shoot() {
         return new SequentialCommandGroup(
-            new SetShooter(120),
+            new SetShooter(110),
              new SetIndexer(IndexerStates.INDEX),
             Commands.waitSeconds(1.0),
             new SetShooter(0),
